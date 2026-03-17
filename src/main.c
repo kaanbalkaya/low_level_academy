d8ee88782c7e298510925f2e5b1d3c84c4297ad3
kaanbalkaya <kaangal@gmail.com>
2026-03-17 15:14:14 +0300

    new file:   Makefile 	new file:   include/kv.h 	new file:   src/kv.c 	new file:   src/main.c

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <kv.h>
+#include <assert.h>
+int main(){
+
+	kv_t *kv=kv_init(3);
+
+	if(kv==NULL){
+		return -1;
+	}
+
+	printf("%p\n",kv);
+	printf("%ld\n",kv->capacity);
+
+	kv_t *db = kv_init(16);
+	assert(db != NULL);
+	assert(db->capacity == 16);
+	assert(db->count == 0);
+	//kv_free(db);
+	return 0;
+}
